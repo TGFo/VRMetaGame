9821b187fc6b3c16f1fc9304716875dad136f684
TGFo <102477641+TGFo@users.noreply.github.com>
2025-05-07 16:55:22 +0200

    gameplaymaybe

---
+++ Assets/Scripts/GhostThrowPost.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class GhostThrowPost : MonoBehaviour
+{
+    public string enemyTag;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag(enemyTag))
+        {
+            Debug.Log("Destroy");
+            Destroy(other.gameObject);
+        }
+    }
+}
--- Assets/Scripts/SpawnGhost.cs
+++ Assets/Scripts/SpawnGhost.cs
@@ -1,9 +1,12 @@
 using Meta.XR.MRUtilityKit;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnGhost : MonoBehaviour
 {
     public GameObject ghostPrefab;
+    public GameObject destroyerPrefab;
     public float spawnTimer = 1;
     private float timer = 0;
     public float minEdgeDistance = .3f;
@@ -11,12 +14,17 @@ public class SpawnGhost : MonoBehaviour
     public MRUKAnchor.SceneLabels spawnLable;
     public MRUK.SurfaceType surface;
     public int spawnTry = 1000;
+    public int maxSpawnedGhostPosts = 5;
+    public int spawnCount = 0;
+    public RuntImeNavmeshBuilder navBuilder;
+    public List<Vector3> spawnPositions = new List<Vector3>();
+    [SerializeField]int failSafeCount = 0;
+    [SerializeField]int failsafeCountMax = 1000;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        StartCoroutine(WaitForSpawn());
     }
-
     // Update is called once per frame
     void Update()
     {
@@ -54,5 +62,36 @@ public class SpawnGhost : MonoBehaviour
             }
         }
 
+    }
+    public IEnumerator WaitForSpawn()
+    {
+        yield return new WaitForSeconds(1);
+        SpawnGhostPosts();
+    }
+    public void SpawnGhostPosts()
+    {
+        Debug.Log("posts");
+        MRUKRoom room = MRUK.Instance.GetCurrentRoom();
+        if (room == null) { Debug.Log("null"); return; }
+        Debug.Log("room");
+        while(spawnCount <= maxSpawnedGhostPosts)
+        {
+            bool hasFoundPosition = room.GenerateRandomPositionOnSurface(surface, minEdgeDistance, new LabelFilter(spawnLable), out Vector3 pos, out Vector3 norm);
+            if(failSafeCount >= failsafeCountMax) return;
+            if (hasFoundPosition)
+            {
+                Debug.Log("spawned");
+                Vector3 randomPositionNormalOffset = pos + norm * normalOffset;
+                randomPositionNormalOffset.y = 0;
+                spawnPositions.Add(randomPositionNormalOffset);
+                Instantiate(destroyerPrefab, randomPositionNormalOffset, Quaternion.identity);
+                spawnCount++;
+            }
+            else
+            {
+                failSafeCount++;
+            }
+        }
+        navBuilder.BuildNav();
     }
 }
